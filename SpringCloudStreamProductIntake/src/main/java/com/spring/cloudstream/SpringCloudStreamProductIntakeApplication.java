98d544659f70b11dfb7d04f9b742c5bad33e09da
Pinaki Sahoo <55279124+PinakiSahoo@users.noreply.github.com>
2019-12-04 16:22:11 +0530

    Add files via upload

---
+++ SpringCloudStreamProductIntake/src/main/java/com/spring/cloudstream/SpringCloudStreamProductIntakeApplication.java
@@ -0,0 +1,13 @@
+package com.spring.cloudstream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudStreamProductIntakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudStreamProductIntakeApplication.class, args);
+	}
+
+}
---
+++ SpringCloudStreamProductIntake/src/test/java/com/spring/cloudstream/SpringCloudStreamProductIntakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.cloudstream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudStreamProductIntakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
